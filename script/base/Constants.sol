11cef224343c4c9b806a8b4f7f37867271ebd38c
saucepoint <saucepoint@protonmail.com>
2025-01-26 16:40:50 -0500

    base addresses

--- script/base/Constants.sol
+++ script/base/Constants.sol
@@ -10,7 +10,7 @@ contract Constants {
     address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
 
     /// @dev populated with default anvil addresses
-    IPoolManager constant POOLMANAGER = IPoolManager(address(0x5FbDB2315678afecb367f032d93F642f64180aa3));
-    PositionManager constant posm = PositionManager(payable(address(0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0)));
+    IPoolManager constant POOLMANAGER = IPoolManager(address(0x498581ff718922c3f8e6a244956af099b2652b2b));
+    PositionManager constant posm = PositionManager(payable(address(0x7c5f5a4bbd8fd63184577525326123b519429bdc)));
     IAllowanceTransfer constant PERMIT2 = IAllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
 }
